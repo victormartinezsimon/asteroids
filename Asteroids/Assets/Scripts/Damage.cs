6ca089fff43967b6cb77f13c9dcd866556a3786a
victor martinez simon <victormartinezsimon@gmail.com>
2017-09-07 00:07:21 +0200

    generate the damage Scripts add the code to generate different asteroids using the levels

---
+++ Asteroids/Assets/Scripts/Damage.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public float _y;
+
+    public float LimitY
+    {
+        set { _y = value; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.localPosition.y < _y)
+        {
+            GameManager.Instance().Damage();
+            PoolManager.Instance().releaseObject(this.gameObject);
+        }
+    }
+}
--- Asteroids/Assets/Scripts/GameManager.cs
+++ Asteroids/Assets/Scripts/GameManager.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
     private static GameManager _instance;
     public static GameManager Instance()
@@ -24,16 +25,75 @@ public class GameManager : MonoBehaviour {
 
     public float _timePlay;
 
-	// Use this for initialization
-	void Awake () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private bool _playingGame = false;
+    public int actualLevel = -31;
+    public float lastAsteroidTime = 0;
+
+    private Vector2 bottomLeft;
+    private Vector2 topRight;
+
+    // Use this for initialization
+    void Awake()
+    {
+        _instance = this;
+        Random.InitState((int)System.DateTime.Now.Ticks);
+
+        bottomLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        topRight = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         UpdateUI();
-        UpdateAsteroids();
-	}
+        if (_playingGame)
+        {
+            UpdateAsteroids(Time.deltaTime);
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                _playingGame = true;
+            }
+        }
+    }
+    void UpdateAsteroids(float time)
+    {
+        _timePlay += time;
+
+        if(_timePlay > 6000)
+        {
+            //the game ended
+            _playingGame = false;
+            return;
+        }
+
+        if(lastAsteroidTime + _levels[actualLevel].timeBetweenLastAsteroid <= _timePlay)
+        {
+            //instatiate a new asteroid
+            GameObject asteroid = PoolManager.Instance().getObject();
+            Movement mv = asteroid.GetComponent<Movement>();
+            mv.Velocity = Random.Range(_levels[actualLevel].velocityMin, _levels[actualLevel].velocityMax);
+
+            Vector3 asteroidSize = asteroid.GetComponent<Renderer>().bounds.size;
+
+            Vector3 initialPosition = Vector3.zero;
+            initialPosition.x = Random.Range(bottomLeft.x + asteroidSize.x, topRight.x - asteroidSize.x);
+            initialPosition.y = topRight.y + asteroidSize.y;
+            asteroid.transform.localPosition = initialPosition; 
+
+            Damage d = asteroid.GetComponent<Damage>();
+            d.LimitY = bottomLeft.y;
+            lastAsteroidTime = _timePlay;
+        }
+
+        while(actualLevel < _levels.Length && _timePlay > _levels[actualLevel + 1].timeActivation)
+        {
+            ++actualLevel;
+        }
+    }
 
     void UpdateUI()
     {
@@ -45,10 +105,6 @@ public class GameManager : MonoBehaviour {
 
     }
 
-    void UpdateAsteroids()
-    {
-
-    }
 
     public void AsteroidDestroyed()
     {
